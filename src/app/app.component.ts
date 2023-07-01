b4e6e803b65d75815a8767f3df7363296d2a8c23
HimanshuYadaavv <yhimanshu71@gmail.com>
2023-07-02 00:04:23 +0530

    AngularHttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {GetdummyService} from "./getdummy.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularHttpClient';
+  email='some@gmail.com'
+
+  constructor(private getDummyService:GetdummyService) {
+  }
+  showData(){
+    this.getDummyService.getData().subscribe((resData)=> this.email=resData.data.email);
+}
+  showOurData(){
+    this.getDummyService.getOurData().subscribe((resOutData)=>this.email=resOutData.email)
+  }
+
+
 }
---
+++ src/app/userdetails.ts
@@ -0,0 +1,7 @@
+export  interface  Userdetails{
+  username:string,
+  firstname:string,
+  lastname:string,
+  email:string,
+  phone:string
+}
